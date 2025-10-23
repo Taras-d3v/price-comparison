f0e95550994b3dfdcf59225a8c16667877dcf209
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-10-24 02:44:14 +0300

    Add BaseCollector file

---
+++ common/pricera/common/collectors/base_collector.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import ClassVar, Optional
+
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+class ScrapyConfigurationMixin:
+    def __init__(self):
+        self.spider_instance = None
+
+    def process_scrapy_spider(
+        self, spider_cls, urls: list[str], proxy_config=None, **kwargs
+    ):
+        process = CrawlerProcess(get_project_settings())
+        crawler = process.create_crawler(spider_cls)
+
+        def handle_spider_opened(spider):
+            self.spider_instance = spider
+
+        crawler.signals.connect(handle_spider_opened, signal=signals.spider_opened)
+
+        process.crawl(crawler, start_urls=urls, proxy_config=proxy_config, **kwargs)
+
+        process.start()  # блокирует выполнение до конца работы паука
+
+        return self.spider_instance
+
+
+@dataclass
+class BaseCollector(ScrapyConfigurationMixin):
+    def crawl(self):
+        raise NotImplemented
+
+    def parse(self, *args, **kwargs) -> dict:
+        raise NotImplemented
+
+    @classmethod
+    def get_collector(cls, *args, **kwargs) -> "BaseCollector":
+        raise NotImplemented
