750a1ec4c6b13e9129b9ba94f799e24b93bae407
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-10-24 02:44:05 +0300

    Add BaseSpider file

---
+++ common/pricera/common/base_scrapy_spider.py
@@ -0,0 +1,30 @@
+import uuid
+from collections import defaultdict
+from dataclasses import dataclass
+
+import scrapy
+from scrapy import Request, Spider
+
+
+class BaseSpider(Spider):
+    custom_settings = {
+        "LOG_ENABLED": True,  # Disable logging for cleaner output
+        "RETRY_TIMES": 3,  # Retry failed requests up to 3 times
+    }
+
+    def __init__(self, *args, **kwargs):
+        Spider.__init__(self, *args, **kwargs)
+        self.responses = defaultdict(list)
+
+    def start_requests(self):
+        """Generate initial requests with chain UUIDs"""
+        if hasattr(self, "start_urls"):
+            for url in self.start_urls:
+                chain_uuid = str(uuid.uuid4())
+                yield scrapy.Request(
+                    url=url,
+                    callback=self.parse,
+                    meta={
+                        "chain_uuid": chain_uuid,
+                    },
+                )
