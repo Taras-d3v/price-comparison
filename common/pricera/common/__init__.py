9b3a3540176db66f08e7c5cb541082aa289d9d8c
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-08 01:23:13 +0200

    Update imports

--- common/pricera/common/__init__.py
+++ common/pricera/common/__init__.py
@@ -12,3 +12,4 @@ from .collectors import BaseCollector, FileBasedMessageConsumer, RabbitMQ
 from .testing_utilities import load_file_from_sub_folder
 from .utilities import ensure_list
 from .mongodb import get_mongo_client
+from .consts import *
